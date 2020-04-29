307304bc224d698b0aaf94bb770a29cd050f71bb
yasu <568200065@qq.com>
2020-04-29 19:02:30 +0900

    更新 leetcode-240

--- [剑指offer-03]findRepeatNumber.go
+++ [剑指offer-03]findRepeatNumber.go
@@ -4,7 +4,7 @@ func findRepeatNumber(nums []int) int {
     m := make(map[int]int, 0)     //声明并初始化map
     n := make([]int, 0)          // 声明并初始化一个存放结果的slice
     for k, v := range nums {     // 遍历nums
-		_, ok := m[v]            // 判断每次遍历的数是否存在于map中，
+        _, ok := m[v]            // 判断每次遍历的数是否存在于map中，
         if ok {                  //若存在，则说明重复，添加该数字到结果slice中
             n = append(n, v) 
         } else {                 //若不存在，则更新map
