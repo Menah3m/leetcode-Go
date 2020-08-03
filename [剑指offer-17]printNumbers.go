c0dc2c5b90be2938fe657dce91091bfe6fb30ed9
yasu <568200065@qq.com>
2020-08-03 22:28:33 +0900

    更新 打印从1到最大的n位数

--- [剑指offer-17]printNumbers.go
+++ [剑指offer-17]printNumbers.go
@@ -1,12 +1,36 @@
-func printNumbers(n int) []int {
-    max := 1
-    for i:= 0; i < n; i++ {
-        max = max * 10
-    }
-    re := make([]int, 0)
-    for i := 1; i < max; i++ {
-        re = append(re, i)
-    }
+"""
+输入数字 n，按顺序打印出从 1 到最大的 n 位十进制数。比如输入 3，则打印出 1、2、3 一直到最大的 3 位数 999。
 
-    return re
+示例 1:
+
+输入: n = 1
+输出: [1,2,3,4,5,6,7,8,9]
+ 
+
+说明：
+
+用返回一个整数列表来代替打印
+n 为正整数
+
+Go
+
+
+
+链接：https://leetcode-cn.com/leetbook/read/illustrate-lcof/xzvgc2/
+来源：剑指offer-17
+
+
+"""
+
+
+func printNumbers(n int) []int {
+    res := make([]int, 0)
+    c := 1
+    for i:=0;i<n;i++{
+        c *= 10
+    }
+    for j:=1;j<c;j++{
+        res = append(res, j)
+    }
+    return res
 }
