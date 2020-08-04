8b7017cd74e843aafe3843f3deda9fc38b71d0ef
yasu <568200065@qq.com>
2020-08-05 01:02:36 +0900

    更新 不用加减乘除做加法

---
+++ [剑指offer-65]adder.py
@@ -0,0 +1,26 @@
+"""
+写一个函数，求两个整数之和，要求在函数体内不得使用 “+”、“-”、“*”、“/” 四则运算符号。
+
+ 
+
+示例:
+
+输入: a = 1, b = 1
+输出: 2
+ 
+
+提示：
+
+a, b 均可能是负数或 0
+结果不会溢出 32 位整数
+
+来源：剑指offer-65
+链接：https://leetcode-cn.com/problems/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof
+"""
+
+func add(a int, b int) int {
+    for b != 0{
+        a, b = a ^ b, a & b << 1
+    }
+    return a
+}
